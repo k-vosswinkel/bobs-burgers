844d2c847ee4a8a6439dbaf53d9559f2d86b3448
Kait Hoehne <kait.hoehne@gmail.com>
2018-02-27 16:40:40 -0500

    Finished category routes

---
+++ server/api/categories.js
@@ -0,0 +1,38 @@
+const router = require('express').Router()
+const { Category } = require('../db/models')
+module.exports = router
+
+router.get('/', (req, res, next) => {
+  Category.findAll()
+    .then(categories => res.json(categories))
+    .catch(next)
+})
+
+router.get('/:categoryId', (req, res, next) => {
+  Category.findById(req.params.categoryId)
+    .then(category => res.json(category))
+    .catch(next)
+})
+
+router.put('/:categoryId', (req, res, next) => {
+  Category.findById(req.params.categoryId)
+    .then(category => category.update(req.body))
+    .then(category => res.json(category))
+    .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+  Category.create(req.body)
+    .then(category => res.json(category))
+    .catch(next)
+})
+
+router.delete('/:categoryId', (req, res, next) => {
+  Category.destroy({
+    where: {
+      id: req.params.categoryId
+    }
+  })
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
