bc7b1b22dcd243aed200d2baf0131d1637213078
manja <anjiemerchant@gmail.com>
2018-03-01 11:30:20 -0500

    adds credential setup

--- script/seed.js
+++ script/seed.js
@@ -19,8 +19,8 @@
 //   // executed until that promise resolves!
 
 //   const users = await Promise.all([
-//     User.create({email: 'cody@email.com', password: '123'}),
-//     User.create({email: 'murphy@email.com', password: '123'})
+//     User.build({email: 'cody@email.com', password: '123'}),
+//     User.build({email: 'murphy@email.com', password: '123'})
 //   ])
 //   // Wowzers! We can even `await` on the right-hand side of the assignment operator
 //   // and store the result that the promise resolves to in a variable! This is nice!
@@ -74,7 +74,7 @@ function doTimes(n, fn) {
 }
 
 const randUser = () => {
-  return User.create({
+  return User.build({
     email: chance.email(),
     password: chance.string({ length: 6 }),
     admin: chance.bool({ likelihood: 10 })
@@ -82,7 +82,7 @@ const randUser = () => {
 }
 
 const randReview = () => {
-  return Review.create({
+  return Review.build({
     text: chance.string({ length: 25 }),
     rating: chance.integer({ min: 1, max: 5 }),
     productId: chance.integer({ min: 1, max: 10 }),
@@ -91,27 +91,27 @@ const randReview = () => {
 }
 
 const randCat = () => {
-  return Category.create({
+  return Category.build({
     name: chance.word()
   })
 }
 
 const randOrderUser = () => {
-  return Order.create({
+  return Order.build({
     userId: chance.integer({ min: 1, max: 50 }),
-    productId: chance.integer({ min: 1, max: 10 })
+    productId: chance.integer({ min: 1, max: 5 })
   })
 }
 
 const randOrderGuest = () => {
-  return Order.create({
-    productId: chance.integer({ min: 1, max: 10 }),
-    status: 'Created'
+  return Order.build({
+    productId: chance.integer({ min: 6, max: 10 }),
+    status: 'buildd'
   })
 }
 
 const randLineItem = () => {
-  return LineItem.create({
+  return LineItem.build({
     quantity: chance.integer({ min: 1, max: 10 }),
     productId: chance.integer({ min: 1, max: 10 }),
     orderId: chance.integer({ min: 1, max: 50 })
@@ -121,61 +121,61 @@ const randLineItem = () => {
 //Hard coded products will go here
 const generateProducts = () => {
   let products = [];
-  products.push(Product.create({
+  products.push(Product.build({
     name: 'Sit and Spinach Burger',
     description: 'This spinach stuffed patty is the best way to eat your greens',
     price: 6.99,
     inventory: 5
   }));
-  products.push(Product.create({
+  products.push(Product.build({
     name: 'Baby You Can Drive My Car! Burger',
     description: 'A feta stuffed burger on a chive-tastic bun. Topped with a million diced chives and a creamy sour cream & mustard spread',
     price: 7.99,
     inventory: 6
   }));
-  products.push(Product.create({
+  products.push(Product.build({
     name: 'Pickle My Funny Bone Burger',
     description: 'Fried pickles take this burger to another level',
     price: 6.75,
     inventory: 10
   }));
-  products.push(Product.create({
+  products.push(Product.build({
     name: 'Do the Brussel Burger',
     description: 'This is the only way you\'ll want to eat Brussel sprouts.',
     price: 8.49,
     inventory: 8
   }));
-  products.push(Product.create({
+  products.push(Product.build({
     name: 'Cheeses is Born Burger',
     description: 'Swiss and Jarlsberg make this cheeseburger extra melty.',
     price: 6.99,
     inventory: 1
   }));
-  products.push(Product.create({
-    name: 'I\'ve Created A Muenster Burger',
+  products.push(Product.build({
+    name: 'I\'ve buildd A Muenster Burger',
     description: 'Who can say no to melty cheese and mushrooms?',
     price: 9.99,
     inventory: 20
   }));
-  products.push(Product.create({
+  products.push(Product.build({
     name: 'It\'s Fun to Eat At The Rye-MCA Burger',
     description: 'All-beef patties on rye bread topped with cheddar cheese, brown mustard, caramelized onions, and horseradish.',
     price: 1.99,
     inventory: 2
   }));
-  products.push(Product.create({
+  products.push(Product.build({
     name: 'The Hard to Find Burger',
     description: 'One of a kind for a reason.',
     price: 8.99,
     inventory: 0
   }));
-  products.push(Product.create({
+  products.push(Product.build({
     name: 'Bet It All On Black Garlic Burger',
     description: 'Winner winnder burger dinner! This all-beef patty is topped with fresh mozarella, spinach, homemade black garlic mayo and a dash of Sriracha hot sauce.',
     price: 12.99,
     inventory: 9
   }));
-  products.push(Product.create({
+  products.push(Product.build({
     name: 'Don\'t You Four Cheddar \'Bout Me Burger',
     description: 'Lettuce, cheeseburger, bacon slices, onions. A gratuitous number of cheddars? No. Five would be crazy. But what are you going to do, three? No. Four\'s your number.',
     price: 4.00,
@@ -208,50 +208,56 @@ const generateLineItem = () => {
   return doTimes(numLineItems, () => randLineItem());
 }
 
-function createUsers() {
+function buildUsers() {
   return Promise.map(generateUsers(), user => user.save());
 }
 
-function createReviews() {
+function buildReviews() {
   return Promise.map(generateReviews(), review => review.save());
 }
 
-function createCategories() {
+function buildCategories() {
   return Promise.map(generateCategories(), category => category.save());
 }
 
-function createOrdersUsers() {
+function buildOrdersUsers() {
   return Promise.map(generateOrderUser(), orderUser => orderUser.save());
 }
 
-function createOrdersGuests() {
+function buildOrdersGuests() {
   return Promise.map(generateOrderGuest(), orderGuest => orderGuest.save());
 }
 
-function createLineItems() {
+function buildLineItems() {
   return Promise.map(generateLineItem(), lineItem => lineItem.save());
 }
 
-function createProducts() {
+function buildProducts() {
   return Promise.map(generateProducts(), product => product.save());
 }
 
-function seed() {
-  return createCategories()
-    .then(createUsers())
-    .then(createProducts())
-    .then(createOrdersUsers())
-    .then(createOrdersGuests())
-    .then(createReviews())
-    .then(createLineItems());
-}
+// function seed() {
+//   return buildCategories()
+//     .then(buildUsers())
+//     .then(buildProducts())
+//     .then(buildOrdersUsers())
+//     .then(buildOrdersGuests())
+//     .then(buildReviews())
+//     .then(buildLineItems());
+// }
 
 console.log('Syncing database');
 
 db.sync({ force: true })
   .then(() => {
     console.log('Seeding database');
-    return seed();
+    return buildCategories()
+      .then(buildUsers())
+      .then(buildProducts())
+      .then(buildOrdersUsers())
+      .then(buildOrdersGuests())
+      .then(buildReviews())
+      .then(buildLineItems());
   })
   .then(() => console.log('Seeding successful'))
   .catch(err => {
