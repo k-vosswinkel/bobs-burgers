e284572c0836d3a91f1e6cb41250ef2f0f5d1de1
Ana C Sanchez <ana.sanchez6547@gmail.com>
2018-03-02 12:46:06 -0500

    add

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,7 @@ const pkg = require('../../package.json')
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  process.env.DATABASE_URL || `postgres://xhlfjtaylianhk:726c5b2d4e70255ea54ce484b4fe885ea9bf82c25b7018f8158182b9a8d8043c@ec2-54-221-207-184.compute-1.amazonaws.com:5432/deoavqeifn1u8u`,
   {
     logging: false
   }
