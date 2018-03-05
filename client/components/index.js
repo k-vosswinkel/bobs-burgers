e5e090e71ded14237ff3697e3e205e4e59bc50c4
Ana C Sanchez <ana.sanchez6547@gmail.com>
2018-03-05 11:53:44 -0500

    changed caps

--- client/components/index.js
+++ client/components/index.js
@@ -4,9 +4,9 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export {default as Navbar} from './Navbar';
-export {default as UserHome} from './User-Home';
-export {Login, Signup} from './Auth-Form';
+export {default as Navbar} from './navbar';
+export {default as UserHome} from './user-home';
+export {Login, Signup} from './auth-form';
 export {default as Reviews} from './Reviews';
 export {default as Cart} from './Cart';
 export {default as Checkout} from './Checkout';
